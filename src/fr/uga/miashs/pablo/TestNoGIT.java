2912f0a4a8afa4f3d939bab7dd99dbc157be2d9a
GunLocked <clement.romanet@hotmail.com>
2023-11-30 13:23:31 +0100

    Création test
    j'ai crée les test pour avancer, tourner et ouvrir/fermer les pinces

--- src/fr/uga/miashs/pablo/TestNoGIT.java
+++ src/fr/uga/miashs/pablo/TestNoGIT.java
@@ -9,8 +9,9 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.utility.Delay;
 
 
+
 public class TestNoGIT {
-	
+
 	public static void run() {
 		UltraSonicSensor ultrasonic = new UltraSonicSensor(SensorPort.S2);
 		ColorSensor color = new ColorSensor(SensorPort.S4);
@@ -19,11 +20,54 @@ public class TestNoGIT {
 		MoteurRoues roues = new MoteurRoues("best");
 		System.out.println("Hello");
 	}
-	
+		
+		public void testPinces(MoteurPinces moteurPinces) {
+			// Test de la fermeture des pinces
+			System.out.println("Fermeture des pinces...");
+			moteurPinces.fermeture();
+
+
+			// Test de l'ouverture des pinces
+			System.out.println("Ouverture des pinces...");
+			moteurPinces.ouverture();
+
+			// Affichage de l'état des pinces après ouverture
+			System.out.println("Les pinces sont-elles ouvertes ? " + moteurPinces.estOuvert());      
+		}
+		public void testVitesse(MoteurRoues moteurRoues) {
+			System.out.println("Avance vite de 10 cm...");
+			moteurRoues.setVitesse("rapide");
+			moteurRoues.avancerT(100);
+			
+			System.out.println("Avance moyennement de 10 cm...");
+			moteurRoues.setVitesse("moyen");
+			moteurRoues.avancerT(100);
+
+			
+			System.out.println("Avance lentement de 10 cm...");
+			moteurRoues.setVitesse("lent");
+			moteurRoues.avancerT(100);
+
+			
+			System.out.println("Avance au mieux de 10 cm...");
+			moteurRoues.setVitesse("best");
+			moteurRoues.avancerT(100);
+		}
+		public void tourner(MoteurRoues moteurRoues){
+			System.out.println("Tourne a droite de 90 degres...");
+			moteurRoues.tourner(80.5, false);
+			
+			System.out.println("Tourne a gauche de 90 degres...");
+			moteurRoues.tourner(-80.5, false);
+		}
+		public void distancePalet(TouchSensor touchS, UltraSonicSensor uSS) {
+			System.out.println("Avance lentement de 10 cm...");
+			
+		}
 
 	public static void main(String[] args) {
 		run();
 	}
-	
+
 
 }
